1391914b6df84e1422bdf9cd72579b2066e2e425
viktor <baranov.viktor.27@gmail.com>
2017-06-26 14:28:39 +0300

    logos fix

--- assets/javascripts/application/index.js
+++ assets/javascripts/application/index.js
@@ -1,12 +1,12 @@
 function getWeb3(callback) {
   if (typeof window.web3 === 'undefined') {
-    // no web3, use fallback
+  	// no web3, use fallback
     console.error("Please use a web3 browser");
     var msgNotEthereum = "You aren't connected to Ethereum. Please, switch on Parity or MetaMask client and refresh the page. Check Oracles network <a href='https://github.com/oraclesorg/oracles-wiki' target='blank'>wiki</a> for more info.";
     swal("Warning", msgNotEthereum, "warning");
     callback(myWeb3, false);
   } else {
-    // window.web3 == web3 most of the time. Don't override the provided,
+  	// window.web3 == web3 most of the time. Don't override the provided,
     // web3, just wrap it in your Web3.
     var myWeb3 = new Web3(window.web3.currentProvider); 
 
@@ -23,7 +23,9 @@ function getWeb3(callback) {
 function checkNetworkVersion(web3, cb) {
 	var msgNotOracles = "You aren't connected to Oracles network. Please, switch on Parity or MetaMask client and choose Oracles network. Check Oracles network <a href='https://github.com/oraclesorg/oracles-wiki' target='blank'>wiki</a> for more info.";
 	web3.version.getNetwork(function(err, netId) {
-		console.log(netId);
+	  if (err)
+	  	console.log(err);
+	  console.log("netId: " + netId);
 	  switch (netId) {
 	    case "1": {
 	      console.log('This is mainnet');
