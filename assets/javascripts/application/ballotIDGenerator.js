07c147d75b40b7fa2569d2206c6f5b59b123ecbc
Igor Dobrovolsky <idobrovolsky.g@gmail.com>
2017-12-12 00:23:01 +0200

    Stub react components

--- assets/javascripts/application/ballotIDGenerator.js
+++
@@ -1,5 +0,0 @@
-function generateBallotID() {
-	var min = 10000000;
-	var max = 99999999;
-  	return Math.floor(Math.random() * (max - min)) + min;
-}
--- modules.js
+++
@@ -1,7 +0,0 @@
-let sweetalert2 = require("sweetalert2");
-let jquery = require("jquery");
-let Web3 = require("web3");
-
-window.swal = sweetalert2;
-window.$ = jquery;
-window.Web3 = Web3;
