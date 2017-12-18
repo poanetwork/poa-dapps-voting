7ac2317ab72f35a9093c5437c1775f5b4cd980c1
viktor <baranov.viktor.27@gmail.com>
2017-12-18 18:22:19 +0300

    Voting contracts interfaces

---
+++ src/votingToChangeKeys.js
@@ -0,0 +1,25 @@
+import votingToChangeKeysABI from './votingToChangeKeysABI.json'
+import Web3 from 'web3';
+import {VOTING_TO_CHANGE_KEYS_ADDRESS} from './addresses'
+
+console.log('VotingToChangeKeys ', VOTING_TO_CHANGE_KEYS_ADDRESS)
+export default class VotingToChangeKeys {
+  constructor(){
+    if(window.web3.currentProvider){
+      let web3_10 = new Web3(window.web3.currentProvider);
+      this.votingToChangeKeysInstance = new web3_10.eth.Contract(votingToChangeKeysABI, VOTING_TO_CHANGE_KEYS_ADDRESS);
+    }
+  }
+
+  createVotingForKeys({startTime, endTime, affectedKey, affectedKeyType, miningKey, ballotType, sender}){
+    return this.votingToChangeKeysInstance.methods.createVotingForKeys(startTime, endTime, affectedKey, affectedKeyType, miningKey, ballotType).send({from: sender})
+  }
+
+  vote({id, choice, sender}){
+    return this.votingToChangeKeysInstance.methods.vote(id, choice).send({from: sender})
+  }
+
+  finalize({id, sender}){
+    return this.votingToChangeKeysInstance.methods.finalize(id, id).send({from: sender})
+  }
+}
---
+++ src/votingToChangeProxy.js
@@ -0,0 +1,25 @@
+import votingToChangeProxyABI from './votingToChangeProxyABI.json'
+import Web3 from 'web3';
+import {VOTING_TO_CHANGE_PROXY} from './addresses'
+
+console.log('VotingToChangeProxy ', VOTING_TO_CHANGE_PROXY)
+export default class VotingToChangeProxy {
+  constructor(){
+    if(window.web3.currentProvider){
+      let web3_10 = new Web3(window.web3.currentProvider);
+      this.votingToChangeKeysInstance = new web3_10.eth.Contract(votingToChangeProxyABI, VOTING_TO_CHANGE_PROXY);
+    }
+  }
+
+  createBallotToChangeProxyAddress({startTime, endTime, proposedValue, sender}){
+    return this.votingToChangeKeysInstance.methods.createVotingForKeys(startTime, endTime, proposedValue).send({from: sender})
+  }
+
+  vote({id, choice, sender}){
+    return this.votingToChangeKeysInstance.methods.vote(id, choice).send({from: sender})
+  }
+
+  finalize({id, sender}){
+    return this.votingToChangeKeysInstance.methods.finalize(id, id).send({from: sender})
+  }
+}
