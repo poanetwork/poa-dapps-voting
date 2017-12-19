0a5ce3fc751635d26d312a2b44e60d85d7c158e0
viktor <baranov.viktor.27@gmail.com>
2017-12-19 18:24:29 +0300

    Favicon, loading screen

---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+const Loading = () => (
+  <div className="loading-container">
+    <div className="loading">
+      <div className="loading-i"></div>
+      <div className="loading-i"></div>
+      <div className="loading-i"></div>
+      <div className="loading-i"></div>
+      <div className="loading-i"></div>
+      <div className="loading-i"></div>
+    </div>
+  </div>
+)
+export default Loading;
