00a08dbd4e5cb4330edb18105e273fab793571ca
viktor <baranov.viktor.27@gmail.com>
2017-12-25 12:18:15 +0300

    Actual social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,10 @@ export const Footer = () => (
       <p className="footer-rights">2017 POA Network. All rights reserved.</p>
       <Link to="/" className="footer-logo"></Link>
       <div className="socials">
-        <a href="#" className="socials-i socials-i_reddit"></a>
-        <a href="#" className="socials-i socials-i_twitter"></a>
-        <a href="#" className="socials-i socials-i_oracles"></a>
-        <a href="#" className="socials-i socials-i_telegram"></a>
-        <a href="#" className="socials-i socials-i_github"></a>
+        <a href="https://twitter.com/oraclesorg" className="socials-i socials-i_twitter"></a>
+        <a href="https://poa.network" className="socials-i socials-i_oracles"></a>
+        <a href="https://t.me/oraclesnetwork" className="socials-i socials-i_telegram"></a>
+        <a href="https://github.com/poanetwork/" className="socials-i socials-i_github"></a>
       </div>
     </div>
   </footer>
