b6210476ee4c6610098819d65308b64cba61b343
viktor <baranov.viktor.27@gmail.com>
2018-01-16 19:25:13 +0300

    Codacy suggestions: Strings must use doublequote. (quotes)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
+import React from "react";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
   <footer className="footer">
