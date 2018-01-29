109588f4bcca6aa7c57dd1f5747e4b4d2adc252a
viktor <baranov.viktor.27@gmail.com>
2018-01-29 19:18:21 +0300

    helpers

---
+++ src/contracts/helpers.js
@@ -0,0 +1,45 @@
+import { constants } from "../constants";
+import { messages } from "../messages";
+import swal from 'sweetalert2';
+
+function addressesURL(branch) {
+    const URL = `https://raw.githubusercontent.com/${constants.organization}/${constants.repoName}/${branch}/${constants.addressesSourceFile}`;
+    console.log(URL);
+    return URL;
+}
+
+function ABIURL(branch, contract) {
+    const URL = `https://raw.githubusercontent.com/${constants.organization}/${constants.repoName}/${branch}/abis/${constants.ABIsSources[contract]}`;
+    console.log(URL);
+    return URL;
+}
+
+function getABI(branch, contract) {
+    let addr = ABIURL(branch, contract);
+    return fetch(addr).then(function(response) {
+        return response.json();
+    })
+}
+
+function wrongRepoAlert(addr) {
+    swal("Error!", messages.wrongRepo(addr), "error");
+}
+
+function getBranch(netId) {
+    switch (netId) {
+        case '77':
+            return 'sokol'
+        case '99':
+            return 'core'
+        default:
+            return 'core'
+    }
+}
+
+module.exports = {
+    addressesURL,
+    ABIURL,
+    getABI,
+    wrongRepoAlert,
+    getBranch
+}
