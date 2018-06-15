5a370b8eac67e110cd320166106035840db3266a
Vadim Arasev <varasev@gmail.com>
2018-06-15 14:12:47 +0300

    (Feature) "Load More" button for "All" tab

--- src/components/Ballots.jsx
+++ src/components/Ballots.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { observable } from "mobx";
 import { inject, observer } from "mobx-react";
 import "babel-polyfill";
 
 @inject("commonStore", "ballotsStore")
 @observer
 export class Ballots extends React.Component {
+  @observable limit;
+
+  constructor(props) {
+    super(props);
+    this.limit = this.props.commonStore.loadMoreLimit;
+    this.step = this.limit;
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick = async () => {
+    const { commonStore } = this.props;
+    this.limit += this.step;
+    commonStore.loadMoreLimit = this.limit;
+  }
+
   componentWillMount () {
     const { commonStore } = this.props;
     commonStore.isActiveFilter = this.props.isActiveFilter;
@@ -12,14 +28,22 @@ export class Ballots extends React.Component {
   }
 
   render () {
-    const { ballotsStore } = this.props;
-    let filteredBallotCards = ballotsStore.ballotCards.toJS().sort((a, b) => {
+    const { ballotsStore, commonStore } = this.props;
+    let ballotCards = ballotsStore.ballotCards.toJS().sort((a, b) => {
       return b.props.startTime - a.props.startTime;
     })
+    let loadMoreButton;
+    if (ballotCards.length > this.limit && !commonStore.isActiveFilter && !commonStore.isToFinalizeFilter) {
+      loadMoreButton = <div className="center">
+        <button type="button" className="load-more" onClick={e => this.onClick(e)}>&darr; LOAD MORE BALLOTS &darr;</button>
+      </div>;
+      ballotCards.splice(this.limit);
+    }
     return (
       <section className="container ballots">
         <h1 className="title">Ballots</h1>
-        {filteredBallotCards}
+        {ballotCards}
+        {loadMoreButton}
       </section>
     );
   }
