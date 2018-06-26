b6474818a0594f119812ea010061783c591f273b
Vadim Arasev <varasev@gmail.com>
2018-06-26 09:29:54 +0300

    (Fix) Don't precache index.html

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -321,7 +321,7 @@ module.exports = {
       // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
       navigateFallbackWhitelist: [/^(?!\/__).*/],
       // Don't precache sourcemaps (they're large) and build asset manifest:
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/, /index\.html$/],
     }),
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
