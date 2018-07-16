27ee4ad781164932c988facbfc952fadff149bef
Gabriel Rodríguez Alsina <grodriguez@keetup.com>
2018-07-16 18:49:43 -0300

    (feat) New Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import Socials from './Socials';
 
-export const Footer = ({netId}) => {
-  const footerClassName = netId === '77' ? 'sokol' : '';
+export const Footer = ({ netId }) => {
+
+  const footerClassName = netId === "77" ? "sokol" : "";
+
   return (
-    <footer className={`footer ${footerClassName}`}>
+    <footer className={`footer ${ footerClassName }`}>
       <div className="container">
-        <p className="footer-rights">{moment().format("YYYY")} POA Network. All rights reserved.</p>
-        <Link to="/poa-dapps-voting" className="footer-logo"></Link>
-        <div className="socials">
-          <a href="https://twitter.com/poanetwork" className="socials-i socials-i_twitter"></a>
-          <a href="https://poa.network" className="socials-i socials-i_oracles"></a>
-          <a href="https://t.me/oraclesnetwork" className="socials-i socials-i_telegram"></a>
-          <a href="https://github.com/poanetwork/" className="socials-i socials-i_github"></a>
-        </div>
+        <Link to="/poa-dapps-voting" className="footer-logo" />
+        <p className="footer-rights">{ moment().format("YYYY") } POA Network. All rights reserved.</p>
+        <Socials />
       </div>
     </footer>
   );
-}
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const Header = ({netId}) => {
-	let headerClassName = netId === '77' ? 'sokol' : '';
-	const logoClassName = netId === '77' ? 'header-logo-sokol' : 'header-logo';
-	return(
-		<header className={`header ${headerClassName}`}>
-			<div className="container">
-			<Link to="/poa-dapps-voting" className={logoClassName}></Link>
-			<Link to="/poa-dapps-voting/new" className="header-new-ballot">New ballot</Link>
-			{/*<Link to="/poa-dapps-voting/settings" className="header-settings">Settings</Link>*/}
-			</div>
-		</header>
-	)
+export const Header = ({ netId }) => {
+  let headerClassName = netId === "77" ? "sokol" : "";
+  const logoClassName = netId === "77" ? "header-logo-sokol" : "header-logo";
+  return (
+    <header className={`header ${headerClassName}`}>
+      <div className="container">
+        <Link to="/poa-dapps-voting" className={logoClassName} />
+        <Link to="/poa-dapps-voting/new" className="header-new-ballot">
+          New ballot
+        </Link>
+        {/*<Link to="/poa-dapps-voting/settings" className="header-settings">Settings</Link>*/}
+      </div>
+    </header>
+  );
 };
