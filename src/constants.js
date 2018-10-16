5b3d9740a758adb80511821c85002129aac32bd6
Vadim <varasev@gmail.com>
2018-10-16 15:22:22 +0300

    (Fix) Remove RPCs from src/constants.js

--- src/constants.js
+++ src/constants.js
@@ -28,25 +28,21 @@ constants.getTransactionReceiptInterval = 5000
 constants.NETWORKS = {
   '77': {
     NAME: 'Sokol',
-    RPC: 'https://sokol.poa.network',
     BRANCH: 'sokol',
     TESTNET: true
   },
   '99': {
     NAME: 'Core',
-    RPC: 'https://core.poa.network',
     BRANCH: 'core',
     TESTNET: false
   },
   '79': {
     NAME: 'Dai-Test',
-    RPC: 'http://40.112.48.125',
     BRANCH: 'dai-test',
     TESTNET: true
   },
   '100': {
     NAME: 'Dai',
-    RPC: 'https://dai.poa.network',
     BRANCH: 'dai',
     TESTNET: false
   }
