989b984ff1d5f2ac90520bcde0302787b9a029da
Vadim <varasev@gmail.com>
2019-04-26 11:39:39 +0300

    Additional fixes

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { LogoPOA } from '../LogoPOA'
 import { LogoSokol } from '../LogoSokol'
+import { LogoKovan } from '../LogoKovan'
 import { LogoDai } from '../LogoDai'
 import { constants } from '../../utils/constants'
 
@@ -8,6 +9,8 @@ export const Logo = ({ href = null, extraClass = '', networkBranch = '' }) => {
   switch (networkBranch) {
     case constants.SOKOL:
       return <LogoSokol href={href} extraClass={extraClass} />
+    case constants.KOVAN:
+      return <LogoKovan href={href} extraClass={extraClass} />
     case constants.DAI:
       return <LogoDai href={href} extraClass={extraClass} />
     default:
