ee855bad52a92df0d2375fd93531a7f5a117ef63
Vadim <varasev@gmail.com>
2019-04-26 11:53:31 +0300

    Fix Kovan RPC address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -59,7 +59,7 @@ constants.KOVAN = 'kovan'
 constants.NETWORKS = {
   '42': {
     NAME: 'Kovan',
-    RPC: 'https://kovan.infura.io/',
+    RPC: 'https://kovan.infura.io/v3/1125fe73d87c4e5396678f4e3089b3dd',
     BRANCH: constants.KOVAN,
     TESTNET: true
   },
